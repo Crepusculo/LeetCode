f1707f7bac855e9c23c3266c49259e86faac8272
Graham Tang <airfree1452@gmail.com>
2016-05-27 01:41:38 +0800

    Proposed the second method

--- LeetCode/136_Single_Number.py
+++ LeetCode/136_Single_Number.py
@@ -1,5 +1,9 @@
 # -*- coding: UTF-8 -*-
-
+"""
+1: +1s重复元素
+2: 删除重复元素
+删除有额外性能开销, 但是最后不需要遍历
+"""
 class Solution(object):
     def singleNumber(self, nums):
         """
@@ -19,3 +23,24 @@ class Solution(object):
         for i in mdict:
             if mdict[i] == 1:
                 return mdict[i]
+
+
+class Solution2(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        mdict = {}
+        for i in nums:
+            if i in mdict:
+                # exist
+                del mdict[i]
+                pass
+            else:
+                # not exist
+                mdict[i] = 1
+
+        for i in mdict:
+            if mdict[i] == 1:
+                return i
