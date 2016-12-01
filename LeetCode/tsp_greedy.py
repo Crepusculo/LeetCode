43ff9fd2af8a88cf4ff14546143e30488c536424
Graham Tang <airfree1452@gmail.com>
2016-12-01 15:13:05 +0800

    Tsp start

---
+++ LeetCode/tsp_greedy.py
@@ -0,0 +1,18 @@
+# matrix
+matrix = [
+    [-1, 2, 3, 4],
+    [2, -1, 6, 2],
+    [3, 6, -1, 7],
+    [4, 2, 7, -1]
+]
+
+def tsp_g(matrix):
+    n = len(matrix[0])
+    e = []
+    while len(e) < n:
+
+        for
+            min()
+
+if __name__ == '__main__':
+    tsp_g(matrix)
