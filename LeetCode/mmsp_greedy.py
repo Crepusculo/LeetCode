0c7aa6aaeca99f38fc29d87f392afa8037fc6391
Graham Tang <airfree1452@gmail.com>
2016-12-01 15:05:55 +0800

    Optimize Multi-machine Scheduled

--- LeetCode/mmsp_greedy.py
+++ LeetCode/mmsp_greedy.py
@@ -1,11 +1,11 @@
 # num of machine
 m_num = 5
 # array of works cost
-cost = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20]
+cost = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 4]
 
 
 def mmsp(num):
-    machines = [[i, 0, []] for i in range(num)]
+    machines = [["MachineIdx: " + str(i), 0, []] for i in range(num)]
     cost.sort(key=lambda x: x)
     for each in cost:
         target = min(machines, key=lambda x: x[1])
@@ -14,4 +14,7 @@ def mmsp(num):
     return machines
 
 if __name__ == '__main__':
-    print(mmsp(m_num))
+    ret = mmsp(m_num)
+    for i in ret:
+        print(i)
+
